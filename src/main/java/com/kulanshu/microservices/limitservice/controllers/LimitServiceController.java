4fd2642110b6c2d0d528309e7ee4b3fbeb8fa537
kulanshusharma <kulanshusharma@192.168.29.29>
2022-07-15 17:34:08 +0530

    Creating Limit controller and bean

---
+++ src/main/java/com/kulanshu/microservices/limitservice/controllers/LimitServiceController.java
@@ -0,0 +1,15 @@
+package com.kulanshu.microservices.limitservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kulanshu.microservices.limitservice.controllers.beans.LimitsConfiguration;
+
+@RestController
+public class LimitServiceController {
+
+	@GetMapping("/limits")
+	public LimitsConfiguration retriveLimitConfiguration() {
+		return new LimitsConfiguration(1000,1);
+	}
+}
