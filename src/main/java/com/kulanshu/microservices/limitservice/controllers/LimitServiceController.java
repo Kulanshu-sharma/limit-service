32c425b259a020ab99117fc5e32d4e5798194008
kulanshusharma <kulanshusharma@192.168.29.29>
2022-07-15 17:59:08 +0530

    Added Configuration for limits

--- src/main/java/com/kulanshu/microservices/limitservice/controllers/LimitServiceController.java
+++ src/main/java/com/kulanshu/microservices/limitservice/controllers/LimitServiceController.java
@@ -1,15 +1,20 @@
 package com.kulanshu.microservices.limitservice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kulanshu.microservices.limitservice.Configuration;
 import com.kulanshu.microservices.limitservice.beans.LimitsConfiguration;
 
 @RestController
 public class LimitServiceController {
 
+	@Autowired
+	public Configuration configuration;
+	
 	@GetMapping("/limits")
 	public LimitsConfiguration retriveLimitConfiguration() {
-		return new LimitsConfiguration(1000,1);
+		return new LimitsConfiguration(configuration.maximum,configuration.minimum);
 	}
 }
